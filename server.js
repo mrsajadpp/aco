c1fd782d1e804f8c1f2311c54713a82e39f49686
Sajad pp <96676224+mrsajadpp@users.noreply.github.com>
2022-07-31 18:15:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
-app.listen(port, () => {
+var listen = app.listen(port, () => {
     console.log('Server running on port : '+port+'.');
 });
 app.get('/', (req,res) => {
     res.send('Server is up.');
 });
+module.exports = listen;
